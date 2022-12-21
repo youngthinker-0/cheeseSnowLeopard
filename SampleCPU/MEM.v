e630804404863a6fd1fa742c78ac1e2603694352
Young Thinker <2395960194@qq.com>
2022-12-21 21:00:37 +0800

    Young Thinker delete some useless code

--- SampleCPU/MEM.v
+++ SampleCPU/MEM.v
@@ -57,20 +57,21 @@ module MEM(
     assign {
         inst_lb, inst_lbu, inst_lh, inst_lhu, inst_lw, inst_sb, inst_sh, inst_sw
     } = ldst_op;
-    assign mem_result = (sel_rf_res & inst_lb & ex_result[1:0] == 2'b00) ? {{24{data_sram_rdata[7]}},data_sram_rdata[7:0]} :
-                        (sel_rf_res & inst_lb & ex_result[1:0] == 2'b01) ? {{24{data_sram_rdata[15]}},data_sram_rdata[15:8]} :
-                        (sel_rf_res & inst_lb & ex_result[1:0] == 2'b10) ? {{24{data_sram_rdata[23]}},data_sram_rdata[23:16]} :
-                        (sel_rf_res & inst_lb & ex_result[1:0] == 2'b11) ? {{24{data_sram_rdata[31]}},data_sram_rdata[31:24]} :
-                        (sel_rf_res & inst_lbu & ex_result[1:0] == 2'b00) ? {{24'b0}, data_sram_rdata[7:0]} :
-                        (sel_rf_res & inst_lbu & ex_result[1:0] == 2'b01) ? {{24'b0}, data_sram_rdata[15:8]} :
-                        (sel_rf_res & inst_lbu & ex_result[1:0] == 2'b10) ? {{24'b0}, data_sram_rdata[23:16]} :
-                        (sel_rf_res & inst_lbu & ex_result[1:0] == 2'b11) ? {{24'b0}, data_sram_rdata[31:24]} :
-                        (sel_rf_res & inst_lh & ex_result[1:0] == 2'b00) ? {{16{data_sram_rdata[15]}},data_sram_rdata[15:0]} :
-                        (sel_rf_res & inst_lh & ex_result[1:0] == 2'b10) ? {{16{data_sram_rdata[31]}},data_sram_rdata[31:16]} :
-                        (sel_rf_res & inst_lhu & ex_result[1:0] == 2'b00) ? {{16'b0},data_sram_rdata[15:0]} :
-                        (sel_rf_res & inst_lhu & ex_result[1:0] == 2'b10) ? {{16'b0},data_sram_rdata[31:16]} :
-                        (sel_rf_res & inst_lw) ? data_sram_rdata : 32'b0;
-    assign rf_wdata = sel_rf_res & data_ram_en ? mem_result : ex_result;
+    
+    assign mem_result = (inst_lb & ex_result[1:0] == 2'b00) ? {{24{data_sram_rdata[7]}},data_sram_rdata[7:0]} :
+                        (inst_lb & ex_result[1:0] == 2'b01) ? {{24{data_sram_rdata[15]}},data_sram_rdata[15:8]} :
+                        (inst_lb & ex_result[1:0] == 2'b10) ? {{24{data_sram_rdata[23]}},data_sram_rdata[23:16]} :
+                        (inst_lb & ex_result[1:0] == 2'b11) ? {{24{data_sram_rdata[31]}},data_sram_rdata[31:24]} :
+                        (inst_lbu & ex_result[1:0] == 2'b00) ? {{24'b0}, data_sram_rdata[7:0]} :
+                        (inst_lbu & ex_result[1:0] == 2'b01) ? {{24'b0}, data_sram_rdata[15:8]} :
+                        (inst_lbu & ex_result[1:0] == 2'b10) ? {{24'b0}, data_sram_rdata[23:16]} :
+                        (inst_lbu & ex_result[1:0] == 2'b11) ? {{24'b0}, data_sram_rdata[31:24]} :
+                        (inst_lh & ex_result[1:0] == 2'b00) ? {{16{data_sram_rdata[15]}},data_sram_rdata[15:0]} :
+                        (inst_lh & ex_result[1:0] == 2'b10) ? {{16{data_sram_rdata[31]}},data_sram_rdata[31:16]} :
+                        (inst_lhu & ex_result[1:0] == 2'b00) ? {{16'b0},data_sram_rdata[15:0]} :
+                        (inst_lhu & ex_result[1:0] == 2'b10) ? {{16'b0},data_sram_rdata[31:16]} :
+                        inst_lw ? data_sram_rdata : 32'b0;
+    assign rf_wdata = sel_rf_res ? mem_result : ex_result;
 
     assign mem_to_wb_bus = {
         hilo_bus,   // 135:70
